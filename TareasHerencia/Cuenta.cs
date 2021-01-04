31b7c93638469910120f5968566a6368b6b05e24
Gerardo Aguilar <gerardo.aguilar@thinkingpeople.es>
2021-01-04 10:09:37 +0100

    ejer1

---
+++ TareasHerencia/Cuenta.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TareasHerencia
+{
+    class Cuenta
+    {
+        private string titular;
+        private double cantidad;
+
+        public string Titular { get => titular; set => titular = value; }
+        public double Cantidad { get => cantidad; set => cantidad = value; }
+
+
+        public Cuenta()
+        {
+            this.titular = "";
+            this.cantidad = 0;
+        }
+        public Cuenta(string titular)
+        {
+            this.titular = titular;
+            this.cantidad = 0;
+        }
+
+        public Cuenta(string titular, double cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+
+        public void toString()
+        {
+            Console.WriteLine("Titular: {0}. Cantidad: {1}", this.titular, this.cantidad);
+        }
+
+        public void Ingresar(double cantidad)
+        {
+            if (cantidad < 0)
+            {
+
+            }
+            else
+            {
+                this.cantidad = this.cantidad+cantidad;
+            }
+        }
+
+        public void Retirar(double cantidad)
+        {
+            if (this.cantidad - cantidad < 0)
+            {
+                this.cantidad = 0;
+            }
+            else
+            {
+                this.cantidad = this.cantidad - cantidad;
+            }
+        }
+    }
+}
---
+++ TareasHerencia/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TareasHerencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuenta C1 = new Cuenta("Federico");
+            C1.toString();
+            Cuenta C2 = new Cuenta("Alejandro", 253.8);
+            C2.toString();
+            C2.Ingresar(-4);
+            C2.toString();
+            C2.Ingresar(325.4);
+            C2.toString();
+            C2.Retirar(100);
+            C2.toString();
+            C2.Retirar(1000);
+            C2.toString();
+        }
+    }
+}
